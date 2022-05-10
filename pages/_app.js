5f104b93454952a69a25fdf2dda197acc50cddab
byeol <byeol4001@gmail.com>
2022-05-10 22:53:00 +0900

    task: search-console 소유권 추가

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -14,7 +15,17 @@ function MyApp({ Component, pageProps }) {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="cEHMvS7TWSxmTdrHaXujawu14-aIg5SmkC1rSlsbpUE"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
